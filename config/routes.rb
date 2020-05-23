88294dca4a283012f6b68d3d6dd8ce7e0e396bf3
Sjors Provoost <sjors@sprovoost.nl>
2020-05-23 16:53:55 +0200

    Set landing page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :municipalities
   resources :provinces
+
+  root to: "provinces#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

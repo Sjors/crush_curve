c0717d068e5565c24e27d9331bd20a60d8c39edb
Sjors Provoost <sjors@sprovoost.nl>
2020-05-23 14:51:09 +0200

    Render provice case counts in table

--- app/controllers/provinces_controller.rb
+++ app/controllers/provinces_controller.rb
@@ -1,6 +1,16 @@
 class ProvincesController < ApplicationController
   def index
     @provinces = Province.all
-    render component: 'Provinces', props: { provinces: @provinces }
+    render component: 'Provinces', props: {
+      provinces: @provinces,
+      days: Case.where("day >= ?", Date.new(2020,4,9)).distinct.order(day: :asc).pluck(:day).collect {|day|
+        {
+          date: day.to_date,
+          cases: Province.all.collect{|province|
+            province.cases.where(day: day).sum(:new_reports)
+          }
+        }
+      }
+    }
   end
 end
--- app/javascript/components/Provinces.js
+++ app/javascript/components/Provinces.js
@@ -3,11 +3,30 @@ import PropTypes from "prop-types"
 class Provinces extends React.Component {
   render () {
     return (
-      <ul>
-        {this.props.provinces.map(province => (
-          <li key={province.cbs_n}>{ province.name }</li>
-        ))}
-      </ul>
+      <table className="winners">
+        <thead>
+          <tr>
+            <th />
+            {this.props.provinces.map(province => (
+              <th key={province.cbs_n}>{ province.name }</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.days.map(day => (
+            <tr key={ day.date }>
+              <td key={ day.date + "-title" }>
+                { day.date }
+              </td>
+              {day.cases.map((case_count, index) => (
+                <td key={index} className={ case_count > 100 ? "bad" : case_count > 30 ? "mediocre" : case_count > 10 ? "better" : case_count > 0 ? "almost" : "good" }>
+                  { case_count }
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }

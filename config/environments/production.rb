caf477d531082536a91c12de7cc7416b7ad36f68
Sjors Provoost <sjors@sprovoost.nl>
2020-05-25 21:03:34 +0200

    Use :sendmail on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "crush_curve_production"
 
+  config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.

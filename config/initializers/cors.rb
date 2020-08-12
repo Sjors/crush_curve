11858b3520cc0310d488b2d479790d3b4c913426
Sjors Provoost <sjors@sprovoost.nl>
2020-08-12 18:28:45 +0200

    Better CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,3 +6,14 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
 end
+
+Rails.application.config.hosts << "pletdecurve.nl"
+
+Rails.application.config.action_dispatch.default_headers = {
+  'X-Frame-Options' => 'SAMEORIGIN',
+  'X-XSS-Protection' => '1; mode=block',
+  'X-Content-Type-Options' => 'nosniff',
+  'X-Download-Options' => 'noopen',
+  'X-Permitted-Cross-Domain-Policies' => 'none',
+  'Referrer-Policy' => 'strict-origin-when-cross-origin'
+}
